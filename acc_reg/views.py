d3b0aa9160b435da6b4d4c28b6ff5c750326b815
0dejia <569982600@qq.com>
2017-04-13 23:28:57 +0800

    create authenticate

--- acc_reg/views.py
+++ acc_reg/views.py
@@ -3,14 +3,20 @@ from __future__ import unicode_literals
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .form import RegisterForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 def register(request):
 	if request.method == 'POST':
 		form = RegisterForm(request.POST)
 		if form.is_valid():
-			return HttpResponseRedirect('/thanks/')
+			return createUser(request)
 	else:
 		form = RegisterForm()
 
 	return render(request, 'acc_reg/register.html', {'form':form})
+
+def createUser(request):
+	user = User.objects.create_user(request.POST['username'], request.POST['email'], request.POST['password'])
+	user.save()
+	return render(request, 'acc_reg/thank.html', {'text':request.POST['username']})
