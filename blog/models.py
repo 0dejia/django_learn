0a899f4ccbfc0b19774394394c83370fa932489f
0dejia <569982600@qq.com>
2017-04-23 17:26:29 +0800

    Add Blog Articles and show it

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class article(models.Model):
+	title = models.CharField(max_length=40)
+	content_time = models.DateTimeField()
+	content = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from blog.models import article
 # Create your views here.
 
 def index(request):
-	return render(request, 'blog/index.html')
+	all_article = article.objects.all()
+	return render(request, 'blog/index.html', {'articles': all_article})
